882dd75e93681a14a9f146bd10705cd08d34ad00
unknown <eric.puskas@yahoo.com>
2019-10-27 17:37:34 +0200

    Add Path

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const path = require("path");
 const auth = require("./routes/auth");
 const search = require("./routes/search");
 const swaggerUi = require("swagger-ui-express");
