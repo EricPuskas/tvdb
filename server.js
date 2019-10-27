eedd7c314dfafa1bcc6508f4d5535aaa325d9e2f
unknown <eric.puskas@yahoo.com>
2019-10-27 17:20:57 +0200

    Prep for deploy

--- server.js
+++ server.js
@@ -5,11 +5,16 @@ const auth = require("./routes/auth");
 const search = require("./routes/search");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
+const helmet = require("helmet");
+const compression = require("compression");
 const PORT = 3001;
+const time = 2500000000; // 1 month
 
 // Enable middleware to recognise the incoming request object as a JSON Object.
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(helmet());
+app.use(compression());
 
 app.all("*", (req, res, next) => {
   // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
@@ -25,6 +30,20 @@ app.all("*", (req, res, next) => {
 // Router middleware
 app.use("/api/auth", auth);
 app.use("/api/search", search);
+
+app.use(express.static("client/build", { maxAge: time }));
+app.use((req, res, next) => {
+  res.header("Cache-Control", "max-age= 2500000000");
+  next();
+});
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Listen to Server
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
