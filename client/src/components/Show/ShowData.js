f15da0274bca4f76b74dc110231c5650d879c5d7
unknown <eric.puskas@yahoo.com>
2019-10-30 11:33:15 +0200

    Improve ShowData

--- client/src/components/Show/ShowData.js
+++ client/src/components/Show/ShowData.js
@@ -10,24 +10,36 @@ const ShowData = ({
 }) => {
   return (
     <div className="ShowData" data-test="showdata">
-      <p>
-        Status: <strong>{status}</strong>
-      </p>
-      <p>
-        Network: <strong>{network}</strong>
-      </p>
-      <p>
-        Airs:{" "}
-        <strong>
-          Every {airsDayOfWeek} at {airsTime}
-        </strong>
-      </p>
-      <p>
-        Seasons: <strong>{seasons} </strong>
-      </p>
-      <p>
-        Total Episodes: <strong> {episodes}</strong>
-      </p>
+      {status ? (
+        <p>
+          Status: <strong>{status}</strong>
+        </p>
+      ) : null}
+
+      {network ? (
+        <p>
+          Network: <strong>{network}</strong>
+        </p>
+      ) : null}
+
+      {airsDayOfWeek && airsTime ? (
+        <p>
+          Airs:{" "}
+          <strong>
+            Every {airsDayOfWeek} at {airsTime}
+          </strong>
+        </p>
+      ) : null}
+      {seasons ? (
+        <p>
+          Seasons: <strong>{seasons} </strong>
+        </p>
+      ) : null}
+      {episodes ? (
+        <p>
+          Total Episodes: <strong> {episodes}</strong>
+        </p>
+      ) : null}
     </div>
   );
 };
